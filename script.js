bf567b78375da15454cb46437a801ef16a225a53
Агата <agata@MacBook-Pro-User.local>
2020-05-17 11:16:13 +0300

    Correction-js

--- script.js
+++ script.js
@@ -1,40 +1,16 @@
-/*var convert = document.getElementById("convert");
+var convert = document.getElementById("convert");
 var celsii = document.getElementById("celsii");
 var farengeuty = document.getElementById("farengeuty");
-
+ 
 function rabota(){
     var temperatura = Number(celsii.value);
-    farengeuty.value = temperatura * (9 / 5) + 32;
+    farengeuty.value = temperatura * (9 / 5)  + 32;
+    convert.addEventListener("click", rabota);
 }
 
-rabota();
 
 
-convert.addEventListener("click", rabota);
-
-var checkbox = document.getElementById("toggle");
-var lamp = document.getElementById("lamp");
-
-checkbox.addEventListener("click", function(){
-    console.log(checkbox.checked);
-    if(checkbox.checked){
-        alert("кнопка нажата!");
-        lamp.style.backgroundColor = "yellow";
-    }else{
-        alert("кнопка не нажата!");
-        lamp.style.backgroundColor = "black";
-    }
-
-    for(i=0; i < 5; ++i){
-        alert(i);
-    }
-});
-*/
-
-
-
-
-var container3 = document.getElementsByClassName("container3")[0];
+var containerone = document.getElementsByClassName("containerone")[0];
 var row, column, square;
 
 for(row = 0; row < 8; ++row){
@@ -44,6 +20,6 @@ for(row = 0; row < 8; ++row){
             square.classList.add("black")
         }
         square.classList.add("kvadratik");
-        container3.appendChild(square);
+        containerone.appendChild(square);
     }
 }
