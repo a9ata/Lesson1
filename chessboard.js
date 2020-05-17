3088cc338a5460d8db29344f6d0406f95ed0be52
Агата <agata@MacBook-Pro-User.local>
2020-05-17 12:07:00 +0300

    Refactoring

--- chessboard.js
+++ chessboard.js
@@ -1,15 +1,3 @@
-var convert = document.getElementById("convert");
-var celsii = document.getElementById("celsii");
-var farengeuty = document.getElementById("farengeuty");
- 
-function rabota(){
-    var temperatura = Number(celsii.value);
-    farengeuty.value = temperatura * (9 / 5)  + 32;
-    convert.addEventListener("click", rabota);
-}
-
-
-
 var containerone = document.getElementsByClassName("containerone")[0];
 var row, column, square;
 
---
+++ exp.js
@@ -0,0 +1,10 @@
+var convert = document.getElementById("convert");
+var celsii = document.getElementById("celsii");
+var farengeuty = document.getElementById("farengeuty");
+ 
+function rabota(){
+    var temperatura = Number(celsii.value);
+    farengeuty.value = temperatura * (9 / 5) + 32;
+}
+
+convert.addEventListener("click", rabota);
