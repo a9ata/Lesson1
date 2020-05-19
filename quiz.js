bef78be8ac51c115d78db1f8cc3f3863ce9ef2c1
Агата <agata@MacBook-Pro-User.local>
2020-05-19 17:57:57 +0300

    Quiz

---
+++ quiz.js
@@ -0,0 +1,15 @@
+var question = document.getElementById("question");
+var container = document.getElementById("container");
+var questionCouter = 1;
+var variantyIterator;
+
+function loadQuestion(){
+    question.innerHTML = Questions[questionCouter].text;
+}
+
+
+for(variantyIterator = 0; variantyIterator < 4 ; ++variantyIterator){
+    
+}
+
+loadQuestion();
